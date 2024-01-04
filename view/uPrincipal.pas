8101233b22124e51ab706d7aad78c6866f9a2afc
igorjacopidutranewcentury <igorjacopidutra@gmail.com>
2024-01-04 16:15:00 -0300

    Add ConnectDataBase in uPrincipal
    Connect to database in project directory dynamically

--- view/uPrincipal.pas
+++ view/uPrincipal.pas
@@ -5,15 +5,17 @@ interface
 uses
    Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
    System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
-   Vcl.StdCtrls;
+   Vcl.StdCtrls, uDM;
 
 type
    TfrmPrincipal = class(TForm)
       btnCliente: TButton;
       procedure btnClienteClick(Sender: TObject);
+      procedure FormShow(Sender: TObject);
    private
     { Private declarations }
       procedure OpenCliente();
+      procedure ConnectDataBase;
    public
     { Public declarations }
    end;
@@ -37,6 +39,34 @@ begin
       FreeAndNil(frmCliente);
    end;
 
+end;
+
+procedure TfrmPrincipal.ConnectDataBase;
+var
+   pathData: string;
+begin
+   pathData := ExtractFilePath(ParamStr(0));
+   pathData := StringReplace(pathData, 'exe\', '', [rfReplaceAll, rfIgnoreCase]);
+   pathData := IncludeTrailingPathDelimiter(pathData) + 'data\DATA.FDB';
+
+   with DM do
+   begin
+      con.Close;
+
+      con.Params.Clear;
+
+      con.Params.Add('DriverID=FB');
+      con.Params.Add('Database=' + pathData);
+      con.Params.Add('User_Name=SYSDBA');
+      con.Params.Add('Password=masterkey');
+
+      con.Open;
+   end;
+end;
+
+procedure TfrmPrincipal.FormShow(Sender: TObject);
+begin
+   ConnectDataBase();
 end;
 
 procedure TfrmPrincipal.OpenCliente;
